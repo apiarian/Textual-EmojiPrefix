98bd30324ad79e9b578e476f81f7c5d121bd1d21
Aleksandr Pasechnik <al@megamicron.net>
2015-11-23 14:58:57 -0500

    added emojis to the user list

--- Textual-EmojiPrefix/TPIEmojiPrefixPrincipalClass.m
+++ Textual-EmojiPrefix/TPIEmojiPrefixPrincipalClass.m
@@ -88,7 +88,6 @@
 + (NSString *)prefix_emoji_renderTemplate:(NSString *)templateName attributes:(NSDictionary *)templateTokens
 {
     if ([templateTokens objectForKey:@"inlineNicknameMatchFound"]) {
-        NSLog(@"swizzling with messageFragment '%@'", [templateTokens objectForKey:@"messageFragment"]);
         NSMutableDictionary *modifiedTokens = [NSMutableDictionary dictionaryWithDictionary:templateTokens];
         NSString *nickname = [templateTokens objectForKey:@"messageFragment"];
         NSString *emoji = [[TPIEmojiGenerator sharedGenerator] getEmojiForNickname:nickname];
@@ -97,6 +96,53 @@
         return [TVCLogRenderer prefix_emoji_renderTemplate:templateName attributes:modifiedTokens];
     }
     return [TVCLogRenderer prefix_emoji_renderTemplate:templateName attributes:templateTokens];
+}
+
+@end
+
+@implementation TVCMemberListCell (EmojiPrefixes)
+
++ (void) load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(updateDrawing:);
+        SEL swizzledSelector = @selector(prefix_emoji_updateDrawing:);
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+- (void)prefix_emoji_updateDrawing:(id)interfaceObject {
+    [self prefix_emoji_updateDrawing:interfaceObject];
+
+    NSString *nickname = [[self memberPointer] nickname];
+    NSString *emoji = [[TPIEmojiGenerator sharedGenerator] getEmojiForNickname:nickname];
+    
+    NSTextField *textField = [self cellTextField];
+    NSMutableAttributedString *label = [[textField attributedStringValue] mutableCopy];
+    NSMutableString *labelString = [label mutableString];
+    [labelString replaceOccurrencesOfString:nickname
+                                 withString:[NSString stringWithFormat:@"%@ %@", emoji, nickname]
+                                    options:NSLiteralSearch
+                                      range:NSMakeRange(0, [labelString length])];
+    [textField setAttributedStringValue:label];
 }
 
 @end
